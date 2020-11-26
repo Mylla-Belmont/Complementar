4cc4e1858b1ac529029b5da0fd48434a038fcd99
Camila Belmont <camilabelmont.alu@gmail.com>
2020-11-26 15:09:27 -0300

    Testando pacote scanner - if/else

---
+++ Operações/#operações L1 - Formatando data - Definindos casas decimais e operador de módulo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; //importando pacote java para ler valores do teclado
+
+class Data {
+    public static void main(String[] args){
+        int hora, min, dia, mes, ano;
+
+        Scanner ler = new Scanner(System.in); //criando o objeto 'ler' com a classe Scanner
+
+        System.out.println("Digite hora, minuto, dia, mês e ano:");
+        hora =  ler.nextInt();
+        min =  ler.nextInt();
+        dia =  ler.nextInt();
+        mes =  ler.nextInt();
+        ano =  ler.nextInt();
+
+        if(hora == 24) {
+            hora = 0;
+        }else if(hora > 12 ){
+            hora = hora - 12;
+        }
+        
+        System.out.printf("%02d:%02d %02d/%02d/%d", hora, min, dia, mes, ano);
+    }
+}
